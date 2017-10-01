5b6b9fc719e13f6f765254e7e7949af1112265c8
Eugene Y. Q. Shen <eugene@eyqs.ca>
2017-09-30 22:03:25 -0700

    Split frontend into two HTML files

--- read.js
+++ read.js
@@ -2,10 +2,8 @@ let progress_percent = 0;
 
 // start the application
 
-function startApp(source_text) {
-  document.getElementById("source").style.display = "none";
+function startApp() {
   document.getElementById("progress").style.width = `${progress_percent}%`
-  document.getElementById("read").style.display = "block";
   source_text = "Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.";
   document.getElementById("source-text").textContent = source_text;
 }
@@ -18,12 +16,7 @@ function submitAnswer(answer_text) {
 // add all event listeners when ready
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("read").style.display = "none";
-  document.getElementById("source").addEventListener("submit", function (e) {
-    e.preventDefault();
-    startApp(e.target.elements.text.value);
-    e.target.elements.text.value = "";
-  });
+  startApp();
   document.getElementById("answer-box").addEventListener("submit", function (e) {
     e.preventDefault();
     submitAnswer(e.target.elements.text.value);
