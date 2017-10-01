de932f53df60195dd10d70e0999e65bb889d1eec
Eugene Y. Q. Shen <eugene@eyqs.ca>
2017-10-01 10:48:08 -0700

    Establish communication between Flask and JS

--- Application/main.py
+++ Application/main.py
@@ -1,14 +1,42 @@
 from flask import Flask, render_template, request
-from collections import OrderedDict
-import json
+from flask_cors import CORS
 from subprocess import Popen, PIPE
+from collections import defaultdict
+from pattern.en import conjugate
+import json
 
 app = Flask(__name__)
+CORS(app)
+
+class Token:
+    def __init__(self, index, word):
+        self.index = index
+        self.word = word
+        self.children = []
+    def update(self, head, tag, label):
+        self.head = head
+        self.tag = tag
+        self.label = label
+    def add_child(self, index):
+        self.children.append(index)
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+            sort_keys=True, indent=4)
+
+class Sentence:
+    def __init__(self, text):
+        self.text = text
+        self.tokens = []
+        for i, word in enumerate(text[:-1].lower().split(" ")):
+            self.tokens.append(Token(i, word))
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+            sort_keys=True, indent=4)
 
 @app.route('/')
 def index():
     message = "My Name Jeff and I am at a hackathon dying right now."
-    command = ("echo "+message+" | docker run --rm -i brianlow/syntaxnet")
+    command = ("echo "+message+" | sudo docker run --rm -i brianlow/syntaxnet")
     output = Popen([command], stdout=PIPE, shell=True)
     print(output.stdout.read())
     return render_template('index.html')
@@ -37,18 +65,17 @@ def generate_questions():
     errors = ''
     try:
         if request.method == 'POST':
-            post = request.form
+            post = json.loads(request.data.decode('utf-8'))
             if post['data']:
                 data=post['data']
             else:
                 raise ValueError('You did not send data!')
         else:
             raise ValueError('Please POST some data.')
-        data = parse(data)
+        data = parse_source(data)
     except Exception as e:
         errors = str(e)
-    output = OrderedDict([('questions', data), ('errors', errors)])
-    return json.dumps(output)
+    return json.dumps({"data": data, "errors": errors})
 
 """
 requires text data and answer(s) in post
@@ -63,11 +90,11 @@ receives http post, sends data to be parsed, generate hints
 @app.route("/generate_hints", methods=['POST', 'GET'])
 def generate_hints():
     data = ''
-    answer = ''
+    answers = ''
     errors = ''
     try:
         if request.method == 'POST':
-            post = request.form
+            post = json.loads(request.data.decode('utf-8'))
             if post['data']:
                 data=post['data']
             else:
@@ -78,11 +105,10 @@ def generate_hints():
                 raise ValueError('You did not send data')
         else:
             raise ValueError('Please POST some data')
-        data = parse(data, hints)
+        data = parse_answers(data)
     except Exception as e:
         errors = str(e)
-    output = OrderedDict([('hints', data), ('errors', errors)])
-    return json.dumps(output)
+    return json.dumps({"data": data, "errors": errors})
 
 """
 
@@ -94,8 +120,79 @@ requires text data as string
 """
 def parse(input_data):
     sentences = input_data.split('. ')
+    print(sentences)
     return sentences[0]
 
+
+def parse_json(json_string):
+    thing = json.loads(json_string)
+    sentence = Sentence(thing["text"])
+    for i in range(len(sentence.tokens)):
+        t = thing["tokens"][i]
+        sentence.tokens[i].update(t["head"], t["tag"], t["label"])
+        if t["head"] != -1:
+            sentence.tokens[t["head"]].add_child(i)
+    return sentence
+
+
+def get_child_tokens(sentence, index):
+    tokens = set()
+    tokens.add(index)
+    for child_index in sentence.tokens[index].children:
+        tokens.update(get_child_tokens(sentence, child_index))
+    return tokens
+
+
+def parse_source(sentence):
+    question_list = defaultdict(list)
+    answer_list = defaultdict(list)
+
+    child_token_list = []
+    for token in sentence.tokens:
+        child_token_list.append(get_child_tokens(sentence, token.index))
+
+    for i, token in enumerate(sentence.tokens):
+        if token.label == "root":
+            question_tokens = ["what", "did"]
+            for j, t in enumerate(sentence.tokens):
+                if t.label == "nsubj":
+                    for k in child_token_list[j]:
+                        question_tokens.append(sentence.tokens[k].word)
+                    break
+            question_tokens.append(conjugate(
+                token.word, tense = "infinitive"))
+            question = " ".join(question_tokens)
+            question_list[i] = question[0].upper() + question[1:] + "?"
+
+        else:
+            if token.label == "nmod:poss":
+                wh = "whose"
+            elif token.label == "nsubj":
+                wh = "who"
+            elif token.label == "obj":
+                wh = "what"
+            else:
+                continue
+
+            question_tokens = []
+            deleted_tokens = child_token_list[i]
+            for j, t in enumerate(sentence.tokens):
+                if j == token.index:
+                    question_tokens.append(wh)
+                elif j not in deleted_tokens:
+                    question_tokens.append(t.word)
+            question = " ".join(question_tokens)
+            question_list[i] = question[0].upper() + question[1:] + "?"
+
+        index = i
+        while index != -1:
+            answer = " ".join([sentence.tokens[t].word for t in child_token_list[index]])
+            answer_list[i].append(answer[0].upper() + answer[1:] + ".")
+            index = sentence.tokens[index].head
+
+    return question_list;
+
+def parse_answers(sentence):
 
 if __name__=='__main__':
     # TODO: Threading true?
--- Application/static/Scripts/read.js
+++ Application/static/Scripts/read.js
@@ -1,13 +1,32 @@
 const LOCAL_STORAGE_NAME = "source_data";
-let progress_percent = 0;
+let current_question = 0;
+let total_questions;
 let source_style = [];
 let source_words;
+let question_list;
 
 // start the application
 
 function startApp() {
-  document.getElementById("progress").style.width = `${progress_percent}%`
-  source_words = window.localStorage.getItem(LOCAL_STORAGE_NAME).split(" ");
+  document.getElementById("progress").style.width = 0;
+  let source_text = window.localStorage.getItem(LOCAL_STORAGE_NAME);
+  source_words = source.split(" ");
+  fetch("http://localhost:5000/generate_questions", {
+        method: 'POST',
+        headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json',
+                  'Access-Control-Allow-Origin': '*',
+              },
+        body: JSON.stringify({"data": source_text}),
+      })
+      .then(function (a) {
+          return a.json();
+      })
+      .then(function (json) {
+        question_list = json.data;
+        total_questions = question_list.length();
+      });
   for (let i in source_words) {
     if (i % 5 == 0)
       source_style.push("color: red;");
@@ -30,8 +49,25 @@ function drawApp() {
 }
 
 function submitAnswer(answer_text) {
-  progress_percent += 10;
-  document.getElementById("progress").style.width = `${progress_percent}%`
+  const progress_percent = 100 * current_question / total_questions;
+  document.getElementById("progress").style.width = `${progress_percent}%`;
+  current_question += 1;
+  fetch("http://localhost:5000/generate_hints", {
+      method: 'POST',
+      headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({"data": answer_text}),
+    })
+    .then(function (a) {
+        return a.json();
+    })
+    .then(function (json) {
+      document.getElementById("question").innerHTML = json.data;
+      if (json.data
+    })
 }
 
 // add all event listeners when ready
