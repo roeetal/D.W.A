97ac65a706e26b3288552f2b7d1d86a8b95d63e9
Eugene Y. Q. Shen <eugene@eyqs.ca>
2017-10-01 05:59:45 -0700

    Store source in local storage and highlight text

---
+++ index.js
@@ -0,0 +1,12 @@
+const LOCAL_STORAGE_NAME = "source_data";
+
+// add all event listeners when ready
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("source").addEventListener("submit", function (e) {
+    e.preventDefault();
+    window.localStorage.setItem(LOCAL_STORAGE_NAME, e.target.elements.text.value);
+    e.target.elements.text.value = "";
+    window.location.href = "read.html";
+  });
+});
--- read.js
+++ read.js
@@ -1,11 +1,32 @@
+const LOCAL_STORAGE_NAME = "source_data";
 let progress_percent = 0;
+let source_style = [];
+let source_words;
 
 // start the application
 
 function startApp() {
   document.getElementById("progress").style.width = `${progress_percent}%`
-  source_text = "Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum. Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.";
-  document.getElementById("source-text").textContent = source_text;
+  source_words = window.localStorage.getItem(LOCAL_STORAGE_NAME).split(" ");
+  for (let i in source_words) {
+    if (i % 5 == 0)
+      source_style.push("color: red;");
+    else if (i % 7 == 0)
+      source_style.push("color: blue;");
+    else
+      source_style.push("color: black;");
+  }
+  drawApp();
+}
+
+// draw the application
+
+function drawApp() {
+  const source_html = [];
+  for (let i in source_words) {
+    source_html.push(`<div class="word" style="${source_style[i]}">${source_words[i]}</div>\n`);
+  }
+  document.getElementById("source-text").innerHTML = source_html.join("");
 }
 
 function submitAnswer(answer_text) {
